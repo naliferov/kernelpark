1b7e5cf8efa293dfdfd29a437adc0fad93d498bd
Nick Aliferov <x8core@gmail.com>
2025-01-18 15:32:51 +0200

    save draft

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-import { IOProcess } from './module/io/io-process.js'
-import ulid from './module/ulid.js'
-
-//console.log(ulid())
-
+//import { IOProcess } from './module/io/io-process.js'
 //import { httpMakeObjectResponse } from './module/io/input/http-stream-utils.js'
+import { stream } from './module/stream.js'
 
-//const httpStream = IOProcess()
+stream.start()
+stream.p({ msg: "oh helloo!" })
+stream.p({ msg: "oh second msg!" })
+
+//stream.s({ msg: "oh helloo!" }, )
+
+//detect is we need to run function and send it event
+
+//let mask = 1 << 30
+//mask = mask | (1 << 2)
+console.log(mask, mask.toString(2))
 
 // httpStream.setHandler((rq) => {
 //     return httpMakeObjectResponse({ prop1: 'monkey' })
---
+++ module/stream.js
@@ -0,0 +1,46 @@
+//import ulid from './ulid.js'
+
+export const stream = {
+    events: new Set,
+
+    p(event) {
+        //const id = ulid()
+        //event.id = id
+        this.events.add(event)
+    },
+    s(event) {
+        
+    },
+    async start() {
+
+        let isProcessing = false
+
+        const processing = async () => {
+            isProcessing = true
+
+            const values = this.events.values()
+            while (true) {
+                const object = values.next()
+                if (object.done) {
+                    break
+                }
+                const event = object.value
+
+                //technique of find find handler
+
+                console.log('processing event', event)
+                this.events.delete(event)
+            }
+
+            isProcessing = false
+        }
+
+        setInterval(() => {
+            if (isProcessing) {
+                return
+            }
+            console.log('run processing events:', new Date)
+            processing()
+        }, 2000)
+    },
+}
